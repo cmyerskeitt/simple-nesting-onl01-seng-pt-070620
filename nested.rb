73e3dbbb29c55de8892da4e0c16f7d99caa5067d
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-17 17:06:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,8 +38,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
+ 
+  
 def dennis_ritchies_language
 	programmer_hash = 
  		{
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
@@ -101,7 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    
      
 end
 
